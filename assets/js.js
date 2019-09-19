5de6e11c0c9ff40ac9aa68c45df732b887cc36f9
crzdvl <vik.coder@gmail.com>
2019-09-19 22:55:12 +0300

    Update js.js

--- assets/js.js
+++ assets/js.js
@@ -7,12 +7,12 @@ function validateName() {
     document.getElementById("userName").style.cssText=`    
       border-color: #ff0000;
     `;
-    document.getElementById("submit_1").disabled = true;
+    return false;
   } else if (document.getElementById("userName").value.match(/^[a-zA-Z0-9]+$/g)){
     document.getElementById("userName").style.cssText=`    
       border-color: #d3bb89;
   `;
-  document.getElementById("submit_1").disabled = false;
+    return true;
   }
 }
 
@@ -22,12 +22,12 @@ function validateHouses() {
     document.getElementById("houses").style.cssText=`    
       border-color: #ff0000;
     `;
-    document.getElementById("submit_1").disabled = true;
+    return false; 
   } else {
     document.getElementById("houses").style.cssText=`    
     border-color: #d3bb89;
   `;
-    document.getElementById("submit_1").disabled = false;
+    return true;
   }
 }
 
@@ -36,41 +36,50 @@ document.getElementById("userEmail").addEventListener("keydown", validateEmail);
 document.getElementById("userPassword").addEventListener("keydown", validatePassword);
 
 function validateEmail() {
+
   if (!document.getElementById("userEmail").value.match(/^[a-zA-Z0-9.!#$%&'*+=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/g)) {
     document.getElementById("userEmail").style.cssText=`    
       border-color: #ff0000;
     `;
-    document.getElementById("submit_2").disabled = true;
 
   } else if (document.getElementById("userEmail").value.match(/^[a-zA-Z0-9.!#$%&'*+=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/g)){
     document.getElementById("userEmail").style.cssText=`    
       border-color: #d3bb89;
   `;
-  document.getElementById("submit_2").disabled = false;
 
   }
 }
 
 function validatePassword() {
+
   if (!document.getElementById("userPassword").value.match(/?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/g)) {
     document.getElementById("userPassword").style.cssText=`    
       border-color: #ff0000;
     `;
-    document.getElementById("submit_2").disabled = true;
+
   } else if (document.getElementById("userPassword").value.match(/?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/g)){
     document.getElementById("userPassword").style.cssText=`    
       border-color: #d3bb89;
   `;
-  document.getElementById("submit_2").disabled = false;
+
   }
 }
 
 submit_1.onclick = function() {
-
-  document.getElementById("firstSection").style.cssText=`    
+  validateName();
+  validateHouses()
+  if(validateName() === true && validateHouses() === true){
+    document.getElementById("firstSection").style.cssText=`    
     display: none;
-`;
-  document.getElementById("secondSection").style.cssText=`    
-    display: block;
-  `;
+    `;
+    document.getElementById("secondSection").style.cssText=`    
+      display: block;
+    `;
+  }
+
+  
+
+
+
+
 }
